7c09ec8c750d276c26bde319d3ec01057ba6b783
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2021-04-21 20:06:12 +0200

    read data

---
+++ hungarianAlgorithm/hungarianAlgorithm/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace hungarianAlgorithm
+{
+    public class Point
+    {
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
