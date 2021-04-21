aa1290cdd79482fbc505e8c119763dcd7a0351f0
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2021-04-21 19:49:31 +0200

    init

---
+++ hungarianAlgorithm/hungarianAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hungarianAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
