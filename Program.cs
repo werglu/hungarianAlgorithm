19553e52b94f89781ace995f477269ed3ea25a6c
Bednarek Mikołaj <mikolaj.bednarek@grupawp.pl>
2021-04-21 20:05:29 +0200

    Created basic project structure
    1. Created solution and project using Rider

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hungarianAlgorithm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
